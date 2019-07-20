24d4c62af07e28701a5e741d7c65a12b5f226e53
SPADTCC <psingh9@dtcc.edu>
2019-07-20 15:25:39 -0400

    SPA Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from '../home/home.component';
 import { MenuComponent } from '../menu/menu.component';
+import { ContactComponent } from '../contact/contact.component';
 
 
 export const routes: Routes = [
-  { path: 'home',  component: HomeComponent },
-  { path: 'menu',     component: MenuComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  {path: 'home', component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'contactus', component: ContactComponent}
 ];
